3bc4aef1b471dbd8da2cc7e5c2538d9f8a7295f0
Huzaifa Khan <134222820+huzaifa11002@users.noreply.github.com>
2025-03-08 03:13:14 +0500

    Update index.py

--- index.py
+++ index.py
@@ -1,100 +1,172 @@
-import streamlit as st
+import streamlit as st 
+import random
+
+# Page Config
+
+st.set_page_config(page_title="Growth Mindset Challenge", layout="centered")
+
+# Sidebar
+st.sidebar.header("What do you want to perform")
+page = st.sidebar.radio("Select:", ["Number Guessing Game", "Calculator", "BMI Calculator" , "Word Counter"])
 
 
-if "history" not in st.session_state:
-    st.session_state.history = []
 
-st.title("Unit Converter App")
-selectConvert= st.selectbox('Choose the Convertor',('Foot','Meter','Centimeter','Kilometer','Inch'))
-if selectConvert =="Foot":
-    selectConverted= st.selectbox('Convert into:',('Meter','Centimeter' ,'Kilometer','Inch'))
-    if selectConverted == "Meter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 0.3048
-    elif selectConverted == "Centimeter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 30.48
-    elif selectConverted == "Kilometer":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 0.0003048
+# Number Guessing Game Page
+
+if page == "Number Guessing Game":
+    st.title("Number Guessing Game 🤷‍♂️")
+    st.write("Guess a number between 1 and 10")
+
+    if "guessed" not in st.session_state:
+        st.session_state.guessed = random.randint(1, 10)
+        st.session_state.attempt = 0
+        st.session_state.result = []
+
+    guess = st.number_input("Enter your guessed number", min_value=1, max_value=10, step=1)
+    
+    if st.button("Check"):
+        st.session_state.attempt += 1
+        if guess > st.session_state.guessed:
+            st.warning("You guessed too high!")
+            st.write("Try again! ☹")
+        elif guess < st.session_state.guessed:
+            st.warning("You guessed too low!")
+            st.write("Try again! ☹")
+        else:
+            st.subheader("Congratulations! You guessed it right! 🎉🎊")
+            st.balloons()
+            st.write(f"It took you {st.session_state.attempt} attempts.")
+            st.session_state.result.append(f"Your Guessed number is {guess}. You got it after {st.session_state.attempt} attempts")
+            st.session_state.guessed = random.randint(1, 10)
+            st.session_state.attempt = 0
+    
+    st.markdown("---")
+    st.subheader("Game History")
+    if len(st.session_state.result) == 0:
+        st.write("No game played yet")
     else:
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 12
-elif selectConvert =="Meter":
-    selectConverted= st.selectbox('Convert into:',('Foot','Centimeter' ,'Kilometer','Inch'))
-    if selectConverted == "Foot":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 0.3048
-    elif selectConverted == "Centimeter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 100
-    elif selectConverted == "Kilometer":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 0.001
+        for res in st.session_state.result:
+            st.write(res)
+
+# Calculator Page
+
+elif page == "Calculator":
+    if "calculatorHistory" not in st.session_state:
+        st.session_state.calculatorHistory = []
+    st.title("Calculator 🖩")
+    st.write("Perform basic arithmetic operations")
+    first_input = st.number_input("Enter the first number", step=1)
+    second_input = st.number_input("Enter the second number", step=1)
+    operation = st.selectbox("Select the operation", ["Addition", "Subtraction", "Multiplication", "Division"])
+
+    if st.button("Calculate"):
+        if operation == "Addition":
+            result = first_input + second_input
+            st.success(f"Anwser is {result}")
+            st.session_state.calculatorHistory.append(f"{first_input} + {second_input} = {result}")
+        elif operation == "Subtraction":
+            result = first_input - second_input
+            st.success(f"Anwser is {result}")
+            st.session_state.calculatorHistory.append(f"{first_input} - {second_input} = {result}")
+        elif operation == "Multiplication":
+            result = first_input * second_input
+            st.success(f"Anwser is {result}")
+            st.session_state.calculatorHistory.append(f"{first_input} * {second_input} = {result}")
+        elif operation == "Division":
+            if second_input == 0:
+                st.error("Division by zero is not allowed")
+            else:
+                result = first_input / second_input
+                st.success(f"Anwser is {result}")
+                st.session_state.calculatorHistory.append(f"{first_input} / {second_input} = {result}")
+
+    st.markdown("---")
+
+    col1, col2= st.columns([4,1])
+    with col1:
+        st.subheader("Calculator History")
+    with col2:
+        if st.button("Clear History"):
+            st.session_state.calculatorHistory = []
+
+    if len(st.session_state.calculatorHistory) == 0:
+        st.write("No calculations performed yet")
     else:
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 39.3701
-elif selectConvert =="Centimeter":
-    selectConverted= st.selectbox('Convert into:',('Foot','Meter','Kilometer','Inch'))
-    if selectConverted == "Foot":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 30.48
-    elif selectConverted == "Meter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 100
-    elif selectConverted == "Kilometer":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 100000
-    else:
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 2.54
-elif selectConvert =="Kilometer":
-    selectConverted= st.selectbox('Convert into:',('Foot','Meter','Centimeter','Inch'))
-    if selectConverted == "Foot":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 0.0003048
-    elif selectConverted == "Meter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 0.001
-    elif selectConverted == "Centimeter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 100000
-    else:
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 39370.1
-else:
-    selectConverted= st.selectbox('Convert into:',('Foot','Meter','Centimeter','Kilometer'))
-    if selectConverted == "Foot":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 12
-    elif selectConverted == "Meter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 39.3701
-    elif selectConverted == "Centimeter":
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput * 2.54
-    else:
-        userInput = st.number_input('Enter your value', step=0.1)
-        valueAnswer = userInput / 39370.1
+        for history in st.session_state.calculatorHistory:
+            st.warning(history)
 
-if st.button('Convert', key='convertor'):
-    st.success(f'Your converted value answer is {valueAnswer:.2f} {selectConverted}')
+# BMI Calculator Page
 
-    st.session_state.history.append(f"{userInput:.2f} {selectConvert} = {valueAnswer:.2f} {selectConverted}")
+elif page == "BMI Calculator" :
+    if "bmiCalculatorHistory" not in st.session_state:
+        st.session_state.bmiCalculatorHistory = []
+    col1, col2 = st.columns([2,1])
+    with col1:
+        st.title('BMI Calculator 📱')
+        st.write('This is a simple BMI calculator')
+        weight = st.number_input('Enter your weight (in kg)', step=0.1)
+        height = st.number_input('Enter your height (in m)', step=0.1)
+
+        if st.button('Calculate BMI', key='bmi'):
+            if weight == 0 or height == 0:
+                st.error('Please enter valid values')
+            else:
+                bmi = weight / (height**2)
+                st.write(f'Your BMI is {bmi:.2f}')
+                st.session_state.bmiCalculatorHistory.append(f'Your BMI is {bmi:.2f}')
+                if bmi < 18.5:
+                    st.info('You are underweight')
+                elif bmi >= 18.5 and bmi < 25:
+                    st.balloons()
+                    st.success('You are normal weight')
+                elif bmi >= 25 and bmi < 30:
+                    st.warning('You are overweight')
+                else:
+                    st.error('You are obese')
+    with col2:
+        st.subheader("Converter 🔁")
+        st.write('This is a simple length converter')
+        selectConvert = st.selectbox('Convert from:',('Foot','Meter','Centimeter'))
+        if selectConvert =="Foot":
+            selectConverted= st.selectbox('Convert into:',('Meter','Centimeter'))
+            if selectConverted == "Meter":
+                userInput = st.number_input('Enter your value', step=0.1)
+                valueAnswer = userInput / 3.281
+            else:
+                userInput = st.number_input('Enter your value', step=0.1)
+                valueAnswer = userInput * 30.48
+        elif selectConvert =="Meter":
+            selectConverted= st.selectbox('Convert into:',('Foot','Centimeter'))
+            if selectConverted == "Foot":
+                userInput = st.number_input('Enter your value', step=0.1)
+                valueAnswer = userInput * 3.281
+            else:
+                userInput = st.number_input('Enter your value', step=0.1)
+                valueAnswer = userInput * 100
+        else:
+            selectConverted= st.selectbox('Convert into:',('Foot','Meter'))
+            if selectConverted == "Foot":
+                userInput = st.number_input('Enter your value', step=0.1)
+                valueAnswer = userInput / 30.48
+            else:
+                userInput = st.number_input('Enter your value', step=0.1)
+                valueAnswer = userInput / 100
+        if st.button('Convert', key='convertor'):
+            if userInput == 0:
+                st.error('Please enter valid values')
+            else:
+                st.success(f'Your converted value answer is {valueAnswer:.2f} {selectConverted}')
 
 
-st.markdown("---")
+# Word Counter Page
 
-col1, col2 = st.columns([4,1])
+elif page == "Word Counter":
 
-with col1:
-    st.header("History")
-with col2:
-    if st.button("Clear History"):
-        st.session_state.history = []
+    st.title("Word Counter 📝")
+    st.write("Count the number of words in a text")
 
-if len(st.session_state.history) == 0:
-    st.write("No history yet.")
-else:
-    for item in st.session_state.history:
-        st.warning(item)
+    para = st.text_area("Enter your text here")
+    if st.button("Count"):
+        split_words = para.split()
+        number_of_words = len(split_words)
+        st.success(f"Total number of words: {number_of_words}")
